26df25013703b88df3854ee3b9c123ebcc7a2f8d
Sami-Wafula <samiwafula3257@gmail.com>
2022-11-25 12:56:11 +0300

    edit index js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,35 @@
+
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
 
-app.listen(3000, ()=>{
-    console.log('Server started')
+// const procedure = require('./stored_procedures');
+dotenv.config()
+const db = require('mssql');
+
+db.connect ={
+    // port: process.env.PORT,
+    name: process.env.USER_NAME,
+    password: process.env.USER_PASS,
+    database: process.env.DB_NAME
+}
+
+app.post('/products', (req, res)=>{
+    db.query('INSERT INTO products ')  
+})
+
+app.get('/products', (req, res)=>{
+    db.query('SELECT * FROM products')
+})
+
+app.get('/product/id', (req, res)=>{
+    db.query()
+})
+
+app.get('/products', (req, res)=>{
+    db.query()
+})
+
+app.listen(process.env.PORT || 4000, ()=>{
+    console.log(`Server started on port ${process.env.PORT}`)
 })
